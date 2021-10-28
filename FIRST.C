b348165a415cdbf3f05677008208d1c58ecb31ca
PPETCHIAMMAL <93313738+PPETCHIAMMAL@users.noreply.github.com>
2021-10-28 14:03:16 +0530

    Add files via upload

---
+++ FIRST.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int no1,no2,out1;
+   clrscr();
+   printf("Enter the values");
+   scanf("%d%d",&no1,&no2);
+   out1=no1+no2;
+   printf("sum of values are %d",out1);
+   getch();
+}
